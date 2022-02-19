7b09a6fb1335bfe83249ed5444fe782feb63a7e0
James Petran <jm.ptrn@gmail.com>
2022-02-19 14:29:01 -0600

    created 3 files, completed 2

---
+++ sliding window/fruits_into_baskets.py
@@ -0,0 +1,14 @@
+def fruits_into_baskets(fruits):
+  window_start = 0
+  max_length = 0
+  fruit_frequency = {}
+
+  #try to extend the range [window_start, window_end]
+  for window_end in range(len(fruits)):
+    right_fruit = fruits[window_end]
+    if right_fruit not in fruit_frequency:
+      fruit_frequency[right_fruit] = 0
+    fruit_frequency[right_fruit] += 1
+
+  # shrink the sliding window, until we are left with '2' fruits in the fruit
+  # frequency dictionary
